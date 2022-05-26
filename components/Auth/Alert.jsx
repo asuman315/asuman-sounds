3af2a5899386ecc241b345a56669e53c4cecd25f
asumanwebdev <asumanwebdev@gmail.com>
2022-05-26 19:55:19 +0300

    rewrite history

--- components/Auth/Alert.jsx
+++ components/Auth/Alert.jsx
@@ -12,7 +12,28 @@ const Alert = ({ alert, setAlert }) => {
     return () => clearTimeout(alertTimeOut);
   }, []);
 
-  return <p className={` text-sm font-meduim tracking-wide py-2 px-4 rounded-sm left-2 w-[95%] mx-auto ${type === 'danger' ? 'text-red bg-light-red border-red border-[1px]' : 'text-green bg-light-green border-[1px] border-green'}`}>{msg}</p>;
+  return <p className={` text-sm font-meduim tracking-wide py-3 px-4 rounded-sm left-2 w-[95%] mx-auto ${type === 'danger' ? 'text-red bg-light-red border-red border-[1px]' : 'text-green bg-light-green border-[1px] border-green'}`}>{msg}</p>;
 };
 
-export default Alert;
+export default Alert;  
+//https://github.com/asuman315/asuman-sounds.git
+
+//  #!/bin/sh
+
+//   git filter-branch --env-filter '
+
+//   OLD_EMAIL=asumanwebdev@.gmail.com
+//   CORRECT_NAME=ssendegeya asuman
+//   CORRECT_EMAIL=asumanwebdev@gmail.com
+
+//   if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+//   then
+//   export GIT_COMMITTER_NAME="$CORRECT_NAME"
+//   export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+//   fi
+//   if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+//   then
+//   export GIT_AUTHOR_NAME="$CORRECT_NAME"
+//   export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+//   fi
+//   ' --tag-name-filter cat -- --branches --tags
