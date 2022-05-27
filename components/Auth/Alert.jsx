a5d85711a60371bfba26e3e19711c062ca6f968c
asumanwebdev <asumanwebdev@gmail.com>
2022-05-27 23:47:11 +0300

    edit sign up

--- components/Auth/Alert.jsx
+++ components/Auth/Alert.jsx
@@ -16,24 +16,4 @@ const Alert = ({ alert, setAlert }) => {
 };
 
 export default Alert;  
-//https://github.com/asuman315/asuman-sounds.git
 
-//  #!/bin/sh
-
-//   git filter-branch --env-filter '
-
-//   OLD_EMAIL=asumanwebdev@.gmail.com
-//   CORRECT_NAME=ssendegeya asuman
-//   CORRECT_EMAIL=asumanwebdev@gmail.com
-
-//   if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-//   then
-//   export GIT_COMMITTER_NAME="$CORRECT_NAME"
-//   export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-//   fi
-//   if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-//   then
-//   export GIT_AUTHOR_NAME="$CORRECT_NAME"
-//   export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-//   fi
-//   ' --tag-name-filter cat -- --branches --tags
