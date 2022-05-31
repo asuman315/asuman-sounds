4cbf478e9b42f28fd0d4755172c04e396d400526
asumanwebdev <asumanwebdev@gmail.com>
2022-05-31 23:44:58 +0300

    fix bug

--- components/Auth/Alert.jsx
+++ components/Auth/Alert.jsx
@@ -10,7 +10,7 @@ const Alert = ({ alert, setAlert }) => {
     }, 3500);
 
     return () => clearTimeout(alertTimeOut);
-  }, [alert]);
+  });
 
   return <p className={` text-sm font-meduim tracking-wide py-3 px-4 rounded-sm left-2 w-[95%] mx-auto ${type === 'danger' ? 'text-red bg-light-red border-red border-[1px]' : 'text-green bg-light-green border-[1px] border-green'}`}>{msg}</p>;
 };
