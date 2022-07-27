edcf90858901e44bbfe8f159d99909f1850cd529
asumanwebdev <asumanwebdev@gmail.com>
2022-07-28 01:37:59 +0300

    innitial commit

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -4,19 +4,49 @@ import {FiArrowDownCircle} from 'react-icons/fi';
 import headPhoneImage from '../../public/images/headphones-two.png';
 import { Link } from 'react-scroll';
 
-export default function Hero() {
-const [currentSlide, setCurrentSlide] = useState(0);
+import jblHeadphone from '../../public/images/jbl-3.png';
 
-useEffect(() => {
-const slideInterval = setInterval(() => {
-setCurrentSlide(currentSlide < 2 ? currentSlide + 1 : 0);
-}, 5000);
-//clearInterval(slideInterval);
-return () => clearInterval(slideInterval);
-});
+export default function Hero() {
+// const [currentSlide, setCurrentSlide] = useState(0);
+
+// useEffect(() => {
+// const slideInterval = setInterval(() => {
+// setCurrentSlide(currentSlide < 2 ? currentSlide + 1 : 0);
+// }, 5000);
+// //clearInterval(slideInterval);
+// return () => clearInterval(slideInterval);
+// });
 
 return (
-<section className='w-screen h-screen relative overflow-hidden'>
+  <section className='w-screen h-screen bg-primary-10'>
+    <div className='flex justify-center items-center max-w-5xl mx-auto'>
+      <div className='text-white flex flex-col justify-center md:justify-start px-6 md:p-0 md:pl-12  md:w-[50%] absolute md:static top-[150px] md:mt-28 z-10'>
+        <h1 className='uppercase sm:text-left'>
+          700bt jbl wireless headphones
+        </h1>
+        <h4 className='normal-case text-lg md:text-3xl my-6 leading-12'>
+          Experience natural life like audio and exceptional build quality made
+          for the passionate music enthusiast
+        </h4>
+        <button className='bg-primary-4 text-primary-10 uppercase tracking-wider sm:w-[300px] md:text-xl py-4 my-4'>
+          see product
+        </button>
+      </div>
+      <div className='absolute top-[150px] md:static'>
+        <img
+          src={jblHeadphone.src}
+          alt='inage of jbl headphones'
+          className='w-full'
+        />
+      </div>
+    </div>
+  </section>
+);
+}
+
+
+
+{/* <section className='w-screen h-screen relative overflow-hidden'>
  <div className='h-full flex w-[fit-content] '>
 {heroItems.map((heroItem, index) => {
 const { h3, h1, imageUrl } = heroItem;
@@ -53,7 +83,4 @@ const { h3, h1, imageUrl } = heroItem;
           );
         })}
       </div> 
-    </section>
-
-);
-}
+    </section> */}
