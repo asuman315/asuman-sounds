fd11fa474f67525befee17f0dfa8e8ce87af8780
asumanwebdev <asumanwebdev@gmail.com>
2022-07-28 00:17:02 +0300

    finish design of mobile

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -17,19 +17,32 @@ export default function Hero() {
   // });
 
   return (
-    <section className='w-screen h-screen flex items-center overflow-hidden'>
-      <div className='flex flex-col justify-start pl-12  w-[60%]'>
-        <h1 className='uppercase text-left'>700bt jbl wireless headphones</h1>
-        <h4 className='normal-case text-3xl my-6 leading-12'>Experience natural life like audio and exceptional build quality made for the passionate music enthusiast</h4>
-        <button className='bg-primary-10 uppercase tracking-wider w-[300px] text-xl py-4 my-4'>see product</button>
-      </div>
-      <div className='h-full flex w-[fit-content] '>
-        <img src={jblHeadphone.src} alt='inage of jbl headphones' className='py-16'/>
+    <section className='w-screen h-[90vh] bg-primary-10'>
+      <div className='flex justify-center items-center max-w-5xl mx-auto'>
+        <div className='text-white flex flex-col justify-center md:justify-start px-6 md:p-0 md:pl-12  md:w-[50%] absolute md:static top-[150px] md:mt-28 z-10'>
+          <h1 className='uppercase sm:text-left'>
+            700bt jbl wireless headphones
+          </h1>
+          <h4 className='normal-case text-lg md:text-3xl my-6 leading-12'>
+            Experience natural life like audio and exceptional build quality
+            made for the passionate music enthusiast
+          </h4>
+          <button className='bg-primary-4 text-primary-10 uppercase tracking-wider sm:w-[300px] md:text-xl py-4 my-4'>
+            see product
+          </button>
+        </div>
+        <div className='absolute top-[150px] md:static'>
+          <img
+            src={jblHeadphone.src}
+            alt='inage of jbl headphones'
+            className='w-full'
+          />
+        </div>
       </div>
     </section>
   );
 }
-     
+
 // {
 //   heroItems.map((heroItem, index) => {
 //     const { h3, h1, imageUrl } = heroItem;
