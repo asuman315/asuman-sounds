6dbd2c1323986bc237590266cf1ac37ad094c6ee
asumanwebdev <asumanwebdev@gmail.com>
2022-07-29 14:37:22 +0300

    imported images

--- components/home/Categories.jsx
+++ components/home/Categories.jsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import headphone from '../../assets/images/headphone.png'
+import headsets from '../../assets/images/headset.png'
+import hometheater from '../../assets/images/hometheater.copy.jpg'
+import speaker from '../../assets/images/speaker.png'
+
+const categories = [
+  {name: 'Headphones', image: headphone},
+  {name: 'Headsets', image: headsets},
+  {name: 'Home Theater', image: hometheater},
+  {name: 'Speakers', image: speaker}
+]
 
 const Categories = () => {
   return (
