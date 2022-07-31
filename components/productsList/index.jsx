d3371690ea33a7bc1e44b07eba99e955404368e2
asumanwebdev <asumanwebdev@gmail.com>
2022-07-31 21:18:39 +0300

    fetched data for the product categories and product items

--- components/productsList/index.jsx
+++ components/productsList/index.jsx
@@ -1,11 +1,16 @@
 
 
-const Menu = () => {
+const ProductsList = ({ productsData }) => {
+
+  console.log('Data of products: ', productsData);
+
+  const categoryName = productsData.attributes.name;
+
   return (
     <section>
-      <h1>menu Items</h1>
+      <h1 className="p-5">Products for the {categoryName} category</h1>
     </section>
   )
 }
 
-export default Menu
+export default ProductsList
--- pages/products/category/[categoryId]/index.jsx
+++ pages/products/category/[categoryId]/index.jsx
@@ -1,14 +1,101 @@
-import Menu from "../../../../components/productsList";
-import Cart from "../../../../components/Cart";
+import ProductsList from '../../../../components/productsList';
+import Cart from '../../../../components/Cart';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function ProductsListPage() {
-
- return (
-  <main>
-    <div className="pt-16">
-    <Cart />
-    </div>
-    <Menu />
-  </main>
- )
+export default function ProductsListPage({ productsData }) {
+  return (
+    <main>
+      <div className='pt-16'>
+        <Cart />
+      </div>
+      <ProductsList productsData={productsData}/>
+    </main>
+  );
 }
+
+export const getStaticProps = async (context) => {
+  //The id of the product displayed on the product/details page.
+  const categoryId = context.params.categoryId;
+
+  const client = new ApolloClient({
+    uri: 'https://asmn-shopping-cart.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  //(id: ${productId})
+  const { data } = await client.query({
+    query: gql`
+      query {
+        audiocategory(id: ${categoryId}) {
+          data {
+      id 
+      attributes {
+        name
+        image {
+          data {
+            attributes {
+              url
+            }
+          }
+        }
+        audioproducts {
+          data {
+            id
+            attributes {
+              name
+              price
+              image {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+        }
+      }
+    `,
+  });
+
+  const productsData = data.audiocategory.data;
+
+  return {
+    props: {
+      productsData,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const client = new ApolloClient({
+    uri: 'https://asmn-shopping-cart.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        audiocategories(pagination: { start: 0, limit: 300 }) {
+          data {
+            id
+          }
+        }
+      }
+    `,
+  });
+
+  const productsData = data.audiocategories.data;
+
+  const paths = productsData.map((product) => {
+    return { params: { categoryId: product.id } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
